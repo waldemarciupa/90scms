b9aebec484a76bc4f6fb228a19c405a9c1fb7f2e
waldemarciupa <waldemarciupa90@gmail.com>
2020-07-08 15:46:39 +0200

    connected app with db

--- index.php
+++ index.php
@@ -144,7 +144,7 @@
                         <span class="input-group-btn">
                             <button class="btn btn-default" type="button">
                                 <span class="glyphicon glyphicon-search"></span>
-                        </button>
+                            </button>
                         </span>
                     </div>
                     <!-- /.input-group -->
