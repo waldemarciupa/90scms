13d4849efbf7b5e07e3b6d8b93c6e84c68174fc7
waldemarciupa <waldemarciupa90@gmail.com>
2020-07-09 20:01:37 +0200

    implemented admin site

--- includes/navigation.php
+++ includes/navigation.php
@@ -30,8 +30,12 @@
                 }
 
                 ?>
+                <li>
+                    <a href="admin">
+                        Admin
+                    </a>
+                </li>
             </ul>
-
         </div>
         <!-- /.navbar-collapse -->
     </div>
