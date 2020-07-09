3ee3ce416f4ccb53574d3e97cea239609391ae2d
waldemarciupa <waldemarciupa90@gmail.com>
2020-07-09 12:42:46 +0200

    implemented dynamic sidebar and splited widget

--- includes/navigation.php
+++ includes/navigation.php
@@ -32,18 +32,6 @@
                 ?>
             </ul>
 
-            <!-- <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li> -->
-
-
-
         </div>
         <!-- /.navbar-collapse -->
     </div>
