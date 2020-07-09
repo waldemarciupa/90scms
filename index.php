46f7bc014b84196a55e8d8666c986d1c71176366
waldemarciupa <waldemarciupa90@gmail.com>
2020-07-09 10:14:29 +0200

    added images to app

--- index.php
+++ index.php
@@ -36,12 +36,16 @@
                 <h2>
                     <a href="#"><?php echo $post_title ?></a>
                 </h2>
+
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author ?></a>
                 </p>
+
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+
+                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+
                 <hr>
                 <p><?php echo $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
