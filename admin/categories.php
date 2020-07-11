d6d6e14ea18041afa3649f2188fbce16f7089844
waldemarciupa <waldemarciupa90@gmail.com>
2020-07-11 10:40:15 +0200

    added posibility of adding new categories to db

--- admin/categories.php
+++ admin/categories.php
@@ -17,7 +17,27 @@
                         <small>Author</small>
                     </h1>
                     <div class="col-xs-6">
-                        <form action="">
+                        <?php
+
+                        if (isset($_POST['submit'])) {
+                            $cat_title = $_POST['cat_title'];
+
+                            if ($cat_title == "" || empty($cat_title)) {
+                                echo "This field cannot be empty!";
+                            } else {
+                                $query = "INSERT INTO categories(cat_title) ";
+                                $query .= "VALUE('{$cat_title}') ";
+
+                                $create_category_query = mysqli_query($connection, $query);
+
+                                if (!$create_category_query) {
+                                    die('Query Failed' . mysqli_error($connection));
+                                }
+                            }
+                        }
+
+                        ?>
+                        <form action="" method="post">
                             <div class="form-group">
                                 <label for="cat_title">Category Title</label>
                                 <input class="form-control" type="text" name="cat_title">
