eab6f597c5ad2153270e93cbfcfdbfe70cfcc000
waldemarciupa <waldemarciupa90@gmail.com>
2020-07-26 10:10:57 +0200

    worked navigation to users

---
+++ admin/includes/edit_user.php
@@ -0,0 +1,113 @@
+<?php
+
+if (isset($_GET['p_id'])) {
+    $post_id_to_edit = $_GET['p_id'];
+}
+
+$query = "SELECT * FROM posts WHERE post_id = $post_id_to_edit ";
+$select_post_by_id = mysqli_query($connection, $query);
+
+while ($row = mysqli_fetch_assoc($select_post_by_id)) {
+    $post_id = $row['post_id'];
+    $post_author = $row['post_author'];
+    $post_title = $row['post_title'];
+    $post_category_id = $row['post_category_id'];
+    $post_status = $row['post_status'];
+    $post_image = $row['post_image'];
+    $post_tags = $row['post_tags'];
+    $post_comment_count = $row['post_comment_count'];
+    $post_content = $row['post_content'];
+    $post_date = $row['post_date'];
+}
+
+if (isset($_POST['update_post'])) {
+
+    $post_author = $_POST['post_author'];
+    $post_title = $_POST['post_title'];
+    $post_category_id = $_POST['post_category'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+    $post_content = $_POST['post_content'];
+    $post_tags = $_POST['post_tags'];
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    if (empty($post_image)) {
+        $query = "SELECT * FROM posts WHERE post_id = $post_id_to_edit ";
+        $select_image = mysqli_query($connection, $query);
+
+        while ($row = mysqli_fetch_assoc($select_image)) {
+            $post_image = $row['post_image'];
+        }
+    }
+
+    $query = "UPDATE posts SET post_title = '{$post_title}',
+    post_category_id = {$post_category_id}, post_date = now(), 
+    post_author = '{$post_author}', post_status = '{$post_status}', post_tags = '{$post_tags}', post_content = '{$post_content}', post_image = '{$post_image}' WHERE post_id = {$post_id_to_edit} ";
+
+    $update_post = mysqli_query($connection, $query);
+
+    confirm($update_post);
+}
+?>
+
+<form action="" method="post" enctype="multipart/form-data">
+
+    <div class="form-group">
+        <label for="post_title">Post Title</label>
+        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title" />
+    </div>
+
+    <div class="form-group">
+        <select name="post_category" id="">
+
+            <?php
+
+            $query = "SELECT * FROM categories";
+            $select_categories = mysqli_query($connection, $query);
+
+            confirm($select_categories);
+
+            while ($row = mysqli_fetch_assoc($select_categories)) {
+                $cat_id = $row['cat_id'];
+                $cat_title = $row['cat_title'];
+                echo "<option value='{$cat_id}'>{$cat_title}</option>";
+            }
+
+            ?>
+
+        </select>
+    </div>
+
+    <div class="form-group">
+        <label for="post_author">Post Author</label>
+        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author" />
+    </div>
+
+    <div class="form-group">
+        <label for="post_status">Post Status</label>
+        <input value="<?php echo $post_status; ?>" type="text" class="form-control" name="post_status" />
+    </div>
+
+    <div class="form-group">
+        <img width="100" src="../images/<?php echo $post_image; ?>" alt="">
+        <input type="file" name="image">
+    </div>
+
+    <div class="form-group">
+        <label for="post_tags">Post Tags</label>
+        <input value="<?php echo $post_tags; ?>" type="text" class="form-control" name="post_tags" />
+    </div>
+
+    <div class="form-group">
+        <label for="post_content">Post Content</label>
+        <textarea class="form-control" name="post_content" id="" cols="30" rows="10"><?php echo $post_content; ?>
+        </textarea>
+    </div>
+
+    <div class="form-group">
+        <input type="submit" class="btn btn-primary" name="update_post" value="Update Post">
+    </div>
+
+</form>
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
             <?php
 
-            $query = "SELECT * FROM posts";
+            $query = "SELECT * FROM posts WHERE post_status = 'published' ";
 
             $select_all_posts_query = mysqli_query($connection, $query);
 
@@ -24,36 +24,42 @@
                 $post_author = $row['post_author'];
                 $post_date = $row['post_date'];
                 $post_image = $row['post_image'];
-                $post_content = substr($row['post_content'], 0, 220) . "...";
+                $post_content = substr($row['post_content'], 0, 220);
+                $post_status = $row['post_status'];
+
+                if ($post_status !== 'published') {
+                    echo "<h1 class='text-center'>No posts here.</h1>";
+                } else {
 
             ?>
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+                    <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                    </h1>
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
-                </h2>
+                    <!-- First Blog Post -->
+                    <h2>
+                        <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
+                    </h2>
 
-                <p class="lead">
-                    by <a href="index.php"><?php echo $post_author ?></a>
-                </p>
+                    <p class="lead">
+                        by <a href="index.php"><?php echo $post_author ?></a>
+                    </p>
 
-                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
-                <hr>
+                    <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+                    <hr>
 
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                    <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
 
-                <hr>
-                <p><?php echo $post_content ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <hr>
+                    <p><?php echo $post_content ?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                <hr>
+                    <hr>
 
-            <?php } ?>
+            <?php }
+            } ?>
 
         </div>
 
