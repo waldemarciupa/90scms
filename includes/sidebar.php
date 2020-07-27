4694d94fe9c65f13121a5e19d91433d955db74d6
waldemarciupa <waldemarciupa90@gmail.com>
2020-07-27 22:13:37 +0200

    added login form

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -16,6 +16,25 @@
         <!-- /.input-group -->
     </div>
 
+    <!-- Login -->
+    <div class="well">
+        <h4>Login</h4>
+        <form action="includes/login.php" method="post">
+            <div class="form-group">
+                <input placeholder="Username" name="username" type="text" class="form-control">
+            </div>
+            <div class="input-group">
+                <input placeholder="Password" name="username" type="password" class="form-control">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" name="login" type="submit">
+                        Submit
+                    </button>
+                </span>
+            </div>
+        </form> <!-- search form -->
+        <!-- /.input-group -->
+    </div>
+
     <!-- Blog Categories Well -->
     <div class="well">
 
