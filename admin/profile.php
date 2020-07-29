7a1776a05271bf7f6bd41c6f5871770087f8fb9e
waldemarciupa <waldemarciupa90@gmail.com>
2020-07-29 07:36:59 +0200

    added editing profile

---
+++ admin/profile.php
@@ -0,0 +1,126 @@
+<?php include "includes/admin_header.php"; ?>
+
+<?php
+
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+
+    $query = "SELECT * FROM users WHERE username = '{$username}'";
+    $select_user_profile_query = mysqli_query($connection, $query);
+
+    while ($row = mysqli_fetch_array($select_user_profile_query)) {
+        $user_id = $row['user_id'];
+        $username = $row['username'];
+        $user_password = $row['user_password'];
+        $user_firstname = $row['user_firstname'];
+        $user_lastname = $row['user_lastname'];
+        $user_email = $row['user_email'];
+        $user_image = $row['user_image'];
+        $user_role = $row['user_role'];
+    }
+}
+
+?>
+
+<?php
+
+if (isset($_POST['edit_profile'])) {
+
+    $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $user_role = $_POST['user_role'];
+    $username = $_POST['username'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+
+    $query = "UPDATE users SET user_firstname = '{$user_firstname}',
+    user_lastname = '{$user_lastname}', 
+    user_role = '{$user_role}', username = '{$username}', user_email = '{$user_email}', user_password = '{$user_password}' WHERE username = '{$username}' ";
+
+    $edit_user_query = mysqli_query($connection, $query);
+
+    confirm($edit_user_query);
+}
+?>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php"; ?>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Profile
+                        <small><?php echo $username; ?></small>
+                    </h1>
+
+                    <form action="" method="post" enctype="multipart/form-data">
+
+                        <div class="form-group">
+                            <label for="post_author">Firtstname</label>
+                            <input type="text" value="<?php echo $user_firstname; ?>" class="form-control" name="user_firstname" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="post_status">Lastname</label>
+                            <input type="text" value="<?php echo $user_lastname; ?>" class="form-control" name="user_lastname" />
+                        </div>
+
+
+                        <div class="form-group">
+                            <select name="user_role" id="">
+                                <option value="subscriber"><?php echo $user_role; ?></option>
+                                <?php
+
+                                if ($user_role == 'admin') {
+                                    echo "<option value='subsriber'>subscriber</option>";
+                                } else {
+                                    echo "<option value='admin'>admin</option>";
+                                }
+
+                                ?>
+                            </select>
+                        </div>
+
+                        <!-- 
+<div class="form-group">
+    <label for="post_image">Post Image</label>
+    <input type="file" class="form-control" name="post_image" />
+</div> -->
+
+                        <div class="form-group">
+                            <label for="post_tags">Username</label>
+                            <input type="text" value="<?php echo $username; ?>" class="form-control" name="username" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="post_content">Email</label>
+                            <input type="email" value="<?php echo $user_email; ?>" class="form-control" name="user_email" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="post_content">Password</label>
+                            <input type="password" value="<?php echo $user_password; ?>" class="form-control" name="user_password" />
+                        </div>
+
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" name="edit_profile" value="Edit Profile">
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+    <!-- /#page-wrapper -->
+
+    <?php include "includes/admin_footer.php"; ?>s
