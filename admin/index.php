c92128d18b7d8c4a36d4242f6323776f7d4e5d7e
waldemarciupa <waldemarciupa90@gmail.com>
2020-07-30 08:23:20 +0200

    displaying data from db on chart

--- admin/index.php
+++ admin/index.php
@@ -159,17 +159,25 @@
 
                 function drawChart() {
                     var data = google.visualization.arrayToDataTable([
-                        ['Year', 'Sales', 'Expenses', 'Profit'],
-                        ['2014', 1000, 400, 200],
-                        ['2015', 1170, 460, 250],
-                        ['2016', 660, 1120, 300],
-                        ['2017', 1030, 540, 350]
+                        ['Date', 'Count'],
+
+                        <?php
+
+                        $element_text = ['Active Posts', 'Comments', 'Users', 'Categories'];
+                        $element_count = [$posts_counts, $comments_counts, $users_counts, $categories_counts];
+
+                        for ($i = 0; $i < 4; $i++) {
+                            echo "['{$element_text[$i]}'" . "," . "{$element_count[$i]}],";
+                        }
+
+                        ?>
+
                     ]);
 
                     var options = {
                         chart: {
-                            title: 'Company Performance',
-                            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                            title: '',
+                            subtitle: '',
                         }
                     };
 
@@ -179,7 +187,7 @@
                 }
             </script>
 
-            <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
+            <div id="columnchart_material" style="width: 90%;  height: auto;"></div>
         </div>
         <!-- /.row -->
     </div>
