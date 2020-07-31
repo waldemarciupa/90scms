c94449150020357d94d4cf23ffb40598692caf4f
waldemarciupa <waldemarciupa90@gmail.com>
2020-07-31 07:36:57 +0200

    removed button read more

--- post.php
+++ post.php
@@ -56,7 +56,6 @@
 
                 <hr>
                 <p><?php echo $post_content ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
 
