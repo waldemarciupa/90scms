1dca2cc078e01cc99ddf9d53d01351077b3a0121
waldemarciupa <waldemarciupa90@gmail.com>
2020-07-31 07:49:49 +0200

    displaying post status in select options

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -86,9 +86,24 @@ if (isset($_POST['update_post'])) {
     </div>
 
     <div class="form-group">
+        <select name="post_status" id="">
+            <option value='<?php echo $post_status; ?>'><?php echo $post_status; ?></option>
+
+            <?php
+            if ($post_status == 'published') {
+                echo "<option value='draft'>draft</option>";
+            } else {
+                echo "<option value='published'>published</option>";
+            }
+            ?>
+
+        </select>
+    </div>
+
+    <!-- <div class="form-group">
         <label for="post_status">Post Status</label>
         <input value="<?php echo $post_status; ?>" type="text" class="form-control" name="post_status" />
-    </div>
+    </div> -->
 
     <div class="form-group">
         <img width="100" src="../images/<?php echo $post_image; ?>" alt="">
