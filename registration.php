1692579b186864f48387c2408dbbc62b6a737b3e
waldemarciupa <waldemarciupa90@gmail.com>
2020-08-07 08:18:42 +0200

    added while loop

--- registration.php
+++ registration.php
@@ -18,6 +18,10 @@ if (isset($_POST['submit'])) {
     if (!$select_randsalt_query) {
         die("Query Failed!" . mysqli_error($connection));
     }
+
+    while ($row = mysqli_fetch_array(($select_randsalt_query))) {
+        echo $salt = $row['randSalt'];
+    }
 }
 
 ?>
