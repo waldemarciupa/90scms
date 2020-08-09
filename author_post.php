4f5745e78f977918a44705d3f0bf79a7b80f4a91
waldemarciupa <waldemarciupa90@gmail.com>
2020-08-09 19:49:19 +0200

    related displaying all posts with author

--- author_post.php
+++ author_post.php
@@ -18,9 +18,10 @@
             if (isset($_GET['p_id'])) {
 
                 $the_post_id = $_GET['p_id'];
+                $the_post_author = $_GET['author'];
             }
 
-            $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+            $query = "SELECT * FROM posts WHERE post_author = '{$the_post_author}' ";
 
             $selectAllPosts = mysqli_query($connection, $query);
 
@@ -46,7 +47,7 @@
                 </h2>
 
                 <p class="lead">
-                    by <a href="index.php"><?php echo $post_author ?></a>
+                    All posts by <a href="index.php"><?php echo $post_author ?></a>
                 </p>
 
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
@@ -61,103 +62,7 @@
 
             <?php } ?>
 
-            <!-- Blog Comments -->
-
-            <?php
-
-            if (isset($_POST['create_comment'])) {
-
-                $the_post_id = $_GET['p_id'];
-
-                $comment_author = $_POST['comment_author'];
-                $comment_email = $_POST['comment_email'];
-                $comment_content = $_POST['comment_content'];
-
-                if (!empty($comment_author) && !empty($comment_email) && !empty($comment_content)) {
-
-                    $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ";
-
-                    $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}' , 'unapproved', now())";
-
-                    $create_comment_query = mysqli_query($connection, $query);
-
-                    confirm($create_comment_query);
-
-                    $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
-
-                    $query .= "WHERE post_id = $the_post_id ";
-
-                    $update_comment_count = mysqli_query($connection, $query);
-
-                    confirm($update_comment_count);
-                } else {
-                    echo "<script>alert('Fields cannot be empty!')</script>";
-                }
-            }
-
-            ?>
-
-            <!-- Comments Form -->
-            <div class="well">
-                <h4>Leave a Comment:</h4>
-                <form action="" method="POST" role="form">
-                    <div class="form-group">
-                        <label for="comment_author">Author</label>
-                        <input type="text" class="form-control" name="comment_author">
-                    </div>
-                    <div class="form-group">
-                        <label for="comment_email">Email</label>
-                        <input type="email" class="form-control" name="comment_email">
-                    </div>
-                    <div class="form-group">
-                        <label for="comment_content">Comment</label>
-                        <textarea class="form-control" rows="3" name="comment_content"></textarea>
-                    </div>
-                    <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
-                </form>
-            </div>
-
-            <hr>
-
-            <!-- Posted Comments -->
-
-            <?php
-
-            $query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} ";
-            $query .= "AND comment_status = 'approved' ";
-            $query .= "ORDER BY comment_id DESC ";
-            $select_comment_query = mysqli_query($connection, $query);
-
-            confirm($select_comment_query);
-
-            while ($row = mysqli_fetch_array($select_comment_query)) {
-                $comment_date = $row['comment_date'];
-                $comment_content = $row['comment_content'];
-                $comment_author = $row['comment_author'];
-            ?>
-
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading"><?php echo $comment_author ?>
-                            <small><?php echo $comment_date ?></small>
-                        </h4>
-                        <?php echo $comment_content ?>
-                    </div>
-                </div>
-
-            <?php } ?>
-
-
-
-
-
-        </div>
-
-        <!-- Blog Sidebar Widgets Column -->
+        </div> <!-- Blog Sidebar Widgets Column -->
         <?php include 'includes/sidebar.php' ?>
 
     </div>
