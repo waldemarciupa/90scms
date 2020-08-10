52dd80a5281bc14186253f1f2c956570daefb7d7
waldemarciupa <waldemarciupa90@gmail.com>
2020-08-10 08:40:52 +0200

    clone posts in diffrent order

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -91,7 +91,7 @@ if (isset($_POST['checkBoxArray'])) {
 
             <?php
 
-            $query = "SELECT * FROM posts";
+            $query = "SELECT * FROM posts ORDER BY post_id DESC";
             $select_posts = mysqli_query($connection, $query);
 
             while ($row = mysqli_fetch_assoc($select_posts)) {
