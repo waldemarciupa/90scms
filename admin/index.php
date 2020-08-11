bdd3fd31148df729336012a51182ddd671202fee
waldemarciupa <waldemarciupa90@gmail.com>
2020-08-11 08:47:51 +0200

    implemented logic to display users online

--- admin/index.php
+++ admin/index.php
@@ -16,8 +16,11 @@
     if ($count == null) {
         mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
     } else {
-        mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
+        mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
     }
+
+    $users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time > '$time_out'");
+    $count_user = mysqli_num_rows($users_online_query)
 
     ?>
 
@@ -35,6 +38,7 @@
                         Welcome to Admin
                         <small><?php echo $_SESSION['username'] ?></small>
                     </h1>
+                    <?php echo $count_user; ?>
                 </div>
             </div>
             <!-- /.row -->
