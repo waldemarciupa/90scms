b0fc54de7e5a85d5446846fb435a066bbadfa1e1
waldemarciupa <waldemarciupa90@gmail.com>
2020-08-11 08:33:58 +0200

    working on displaying users online and modified pagination color

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,25 @@
 
 <div id="wrapper">
 
+    <?php
+
+    $session = session_id();
+    $time = time();
+    $time_out_in_seconds = 60;
+    $time_out = $time - $time_out_in_seconds;
+
+    $query = "SELECT * FROM users_online WHERE session = '$session'";
+    $send_query = mysqli_query($connection, $query);
+    $count = mysqli_num_rows($send_query);
+
+    if ($count == null) {
+        mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
+    } else {
+        mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
+    }
+
+    ?>
+
     <!-- Navigation -->
     <?php include "includes/admin_navigation.php"; ?>
 
