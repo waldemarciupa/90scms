6e8e16aac20476bad81222cd0f8cb8510647c99a
waldemarciupa <waldemarciupa90@gmail.com>
2020-08-12 20:08:45 +0200

    refactored counting comments

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -127,7 +127,13 @@ if (isset($_POST['checkBoxArray'])) {
                 echo "<td>$post_status</td>";
                 echo "<td><img width='150' src='../images/{$post_image}'/></td>";
                 echo "<td>$post_tags</td>";
-                echo "<td>$post_comment_count</td>";
+
+                $query = "SELECT * FROM comments WHERE comment_post_id = $post_id";
+                $send_comment_query = mysqli_query($connection, $query);
+                $count_comments = mysqli_num_rows($send_comment_query);
+
+                echo "<td>$count_comments</td>";
+
                 echo "<td>$post_date</td>";
                 echo "<td><a href='../post.php?p_id=$post_id'>View Post</a></td>";
                 echo "<td><a href='posts.php?source=edit_post&p_id=$post_id'>Edit</a></td>";
--- post.php
+++ post.php
@@ -91,13 +91,12 @@
 
                     confirm($create_comment_query);
 
-                    $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
+                    // $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
 
-                    $query .= "WHERE post_id = $the_post_id ";
+                    // $query .= "WHERE post_id = $the_post_id ";
+                    // $update_comment_count = mysqli_query($connection, $query);
 
-                    $update_comment_count = mysqli_query($connection, $query);
-
-                    confirm($update_comment_count);
+                    // confirm($update_comment_count);
                 } else {
                     echo "<script>alert('Fields cannot be empty!')</script>";
                 }
