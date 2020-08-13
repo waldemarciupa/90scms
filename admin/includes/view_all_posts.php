fc935204033df54eeda49e14c5ad3d48ae4ba83b
waldemarciupa <waldemarciupa90@gmail.com>
2020-08-13 08:08:38 +0200

    counting posts

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -130,9 +130,13 @@ if (isset($_POST['checkBoxArray'])) {
 
                 $query = "SELECT * FROM comments WHERE comment_post_id = $post_id";
                 $send_comment_query = mysqli_query($connection, $query);
+
+                $row = mysqli_fetch_array($send_comment_query);
+                $comment_id = $row['comment_id'];
                 $count_comments = mysqli_num_rows($send_comment_query);
 
-                echo "<td>$count_comments</td>";
+
+                echo "<td><a href='comment.php?id=$comment_id'>$count_comments</a></td>";
 
                 echo "<td>$post_date</td>";
                 echo "<td><a href='../post.php?p_id=$post_id'>View Post</a></td>";
